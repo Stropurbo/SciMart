cbd11370efca5aad18ebc54e4722689347b9a1d0
Sabbir Hasan <xofficial490@gmail.com>
2025-04-14 16:22:24 +0600

    root api added

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from product.views import ProductViewset, CategoryViewSet, ReviewViewSet, ProductImageViewSet
 from order.views import CartViewSet, CartItemViewSet, OrderViewSet
 from rest_framework_nested import routers
-from django.http import JsonResponse
 
 router = routers.DefaultRouter()
 router.register('products', ProductViewset, basename="products")
@@ -27,7 +26,4 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
 
-]
-urlpatterns += [
-    path('', lambda request: JsonResponse({"message": "SciMart API is working!"}))
 ]
--- sci_mart/urls.py
+++ sci_mart/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from .views import api_root_view
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -22,7 +23,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api-auth/', include('rest_framework.urls')),
+    path('', api_root_view),
     path('api/v1/', include('api.urls'), name='api-root'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
