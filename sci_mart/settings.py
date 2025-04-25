beaa3878cb710bec581f93b4ed926c938643755b
Sabbir Hasan <xofficial490@gmail.com>
2025-04-25 18:28:08 +0600

    added comma after add in installed apps

--- sci_mart/settings.py
+++ sci_mart/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
+    'corsheaders',
     'drf_yasg',
     'api',
     'order',
