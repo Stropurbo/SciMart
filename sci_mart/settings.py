ace1a6e892f4a43f288a6c192465b69c5413dcbc
Sabbir Hasan <xofficial490@gmail.com>
2025-04-25 21:41:21 +0600

    remove pycog2 from reqyuirements

--- sci_mart/settings.py
+++ sci_mart/settings.py
@@ -170,6 +170,7 @@ REST_FRAMEWORK = {
      'COERCE_DECIMAL_TO_STRING' : False,
      'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
      'PAGE_SIZE' : 10,
+      'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
      'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
