382f2434215d20e13a248402a2708f117f5e22f6
Sabbir Hasan <xofficial490@gmail.com>
2025-04-29 12:42:48 +0600

    is_staff added in user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -6,5 +6,6 @@ class UserCreateSerializers(UserCreateSerializer):
 
 class CurrentUserSerializers(UserSerializer):
     class Meta(UserSerializer.Meta):
-        fields = ['id', 'email','first_name', 'last_name','address','phone_number']
+        fields = ['id', 'email','first_name', 'last_name','address','phone_number', 'is_staff']
+        read_only_fields = ['is_staff']
     
